6cc3d7742a556e7d9442deea299c93ff66d94edc
laminehanane <lamine.hanane@gmail.com>
2024-02-17 11:46:16 -0500

    the new feature sun is added

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -5,6 +5,10 @@ public class SolarSystem {
         Base solar = new Base();
         solar.Stars = "100";
         solar.Moon = "4";
-        solar.Planets = "2";
+
+        FeatureSun xros= new FeatureSun();
+        xros.Color="pink";
+        xros.temp="23243243243234k";
+        xros.Size="234243432432432424miles";
     }
 }
